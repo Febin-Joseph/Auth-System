bc77137e8b3207e163257186cac32b55a48ff863
Febin-Joseph <febinachu123@gmail.com>
2024-03-26 22:34:43 +0530

    api setup

--- authSystem/api/views.py
+++ authSystem/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Item
+from .serializers import ItemSerializer
+from .permissions import IsOwner
 
-# Create your views here.
+class ItemListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class ItemRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
