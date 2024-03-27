3462e61062fdaff38179ec888412232f4f1cc383
Febin-Joseph <febinachu123@gmail.com>
2024-03-27 22:16:09 +0530

    added complete auth

--- authSystem/api/permissions.py
+++ authSystem/api/permissions.py
@@ -1,5 +1,11 @@
 from rest_framework import permissions
 
-class IsOwner(permissions.BasePermission):
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
+        return request.user.is_authenticated and obj.owner == request.user
--- authSystem/api/views.py
+++ authSystem/api/views.py
@@ -1,19 +1,17 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
 from .models import Item
 from .serializers import ItemSerializer
-from .permissions import IsOwner
-
+from .permissions import IsOwnerOrReadOnly
 
 class ItemListCreateAPIView(generics.ListCreateAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-
 class ItemRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-    permission_classes = [permissions.IsAuthenticated, IsOwner]
+    permission_classes = [IsOwnerOrReadOnly]
