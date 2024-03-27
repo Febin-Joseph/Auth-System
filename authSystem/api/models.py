9045ffa970c599960d9b2a382ecfb32667bcf838
Febin-Joseph <febinachu123@gmail.com>
2024-03-27 22:47:10 +0530

    made owner not much required

--- authSystem/api/models.py
+++ authSystem/api/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name
