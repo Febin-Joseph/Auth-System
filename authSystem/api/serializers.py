83304fc3f0793638856547660e884cde3069ebe7
Febin-Joseph <febinachu123@gmail.com>
2024-03-28 13:35:29 +0530

    cannot change owner value

--- authSystem/api/serializers.py
+++ authSystem/api/serializers.py
@@ -4,4 +4,8 @@ from .models import Item
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ['id', 'name', 'owner']
+        fields = ["id", "name", "owner"]
+
+    def update(self, instance, validated_data):
+        validated_data.pop("owner", None)
+        return super().update(instance, validated_data)
